6f6b69bdf84e1bccfea70ebd13474d132b8b029c
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-07-13 16:00:35 +0530

    server setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState("")
+  
+  useEffect(() => {
+    axios.get('http://127.0.0.1:6969/api/hierarchy/getall')
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+      }
+    )
+    .catch(err => {
+      console.log(err)
+    }
+  )
+  },[])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Hello Employees
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {data && data.data.map((item, index) => {
+          return (
+            <div key={index}>
+              {item.Ename}
+            </div>
+          )
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
