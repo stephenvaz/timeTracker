53c0fb47154ce9a1fbc151c9aff4e09e57c9c33c
stephenvaz <stephenvaz2003@gmail.com>
2023-07-15 12:16:31 +0530

    centered tree and animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,64 +1,86 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import Tree from 'react-d3-tree';
-import axios from 'axios'
-// import './App.css'
-import "./tree.css"
+import axios from 'axios';
+import './tree.css';
 
 function App() {
-  const [data, setData] = useState(null)
-  
+  const [data, setData] = useState(null);
+  const [treeTranslate, setTreeTranslate] = useState({ x: window.innerWidth/2, y: window.innerHeight/3 });
+
   useEffect(() => {
-    axios.get('http://127.0.0.1:6969/api/hierarchy/getall')
-      .then(res => {
-        console.log(res.data)
-        const hierarchyData = formatHierarchyData(res.data)
-        console.log(hierarchyData)
-        setData(hierarchyData)
+    axios
+      .get('http://127.0.0.1:6969/api/hierarchy/getall')
+      .then((res) => {
+        console.log(res.data);
+        const hierarchyData = formatHierarchyData(res.data);
+        console.log(hierarchyData);
+        setData(hierarchyData);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleResize = () => {
+    // Update the tree translation based on the new window dimensions
+    const newTranslate = {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 3,
+    };
+    setTreeTranslate(newTranslate);
+  };
 
   const formatHierarchyData = (apiData) => {
     const hierarchyMap = {};
     let root = null;
-  
+
     apiData.data.forEach((item) => {
       const { HierarchyID, EmployeeID, SupervisorID, Ename, Sname } = item;
       hierarchyMap[EmployeeID] = {
         name: Ename,
-        // attributes: {
-        //   department: Sname,
-        // },
         children: [],
       };
     });
-  
+
     apiData.data.forEach((item) => {
       const { EmployeeID, SupervisorID } = item;
       const node = hierarchyMap[EmployeeID];
       const supervisorNode = hierarchyMap[SupervisorID];
-  
+
       if (SupervisorID === EmployeeID) {
         root = node;
       } else if (supervisorNode) {
         supervisorNode.children.push(node);
       }
     });
-  
+
     return root;
   };
-  
+
   return (
-    <div id="treeWrapper" style={{ width: '100vw', height: '100vh'}}>
-      {data && <Tree data={data} orientation='vertical' rootNodeClassName="node__root"
-        branchNodeClassName="node__branch"
-        leafNodeClassName="node__leaf"
-        svgClassName='custom'
-         />}
+    <div id="treeWrapper" style={{ width: '100vw', height: '100vh' }}>
+      {data && (
+        <Tree
+          data={data}
+          orientation="vertical"
+          rootNodeClassName="node__root"
+          branchNodeClassName="node__branch"
+          leafNodeClassName="node__leaf"
+          svgClassName="custom"
+          enableLegacyTransitions={true}
+          translate={treeTranslate}
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
