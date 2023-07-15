4cbd5c22e21d7449781d70bcebd7d21ca9f66aa7
stephenvaz <stephenvaz2003@gmail.com>
2023-07-15 12:00:26 +0530

    tree rep complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,63 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Tree from 'react-d3-tree';
 import axios from 'axios'
-import './App.css'
+// import './App.css'
+import "./tree.css"
 
 function App() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState(null)
   
   useEffect(() => {
     axios.get('http://127.0.0.1:6969/api/hierarchy/getall')
       .then(res => {
         console.log(res.data)
-        setData(res.data)
-      }
-    )
-    .catch(err => {
-      console.log(err)
-    }
-  )
-  },[])
+        const hierarchyData = formatHierarchyData(res.data)
+        console.log(hierarchyData)
+        setData(hierarchyData)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
+  const formatHierarchyData = (apiData) => {
+    const hierarchyMap = {};
+    let root = null;
+  
+    apiData.data.forEach((item) => {
+      const { HierarchyID, EmployeeID, SupervisorID, Ename, Sname } = item;
+      hierarchyMap[EmployeeID] = {
+        name: Ename,
+        // attributes: {
+        //   department: Sname,
+        // },
+        children: [],
+      };
+    });
+  
+    apiData.data.forEach((item) => {
+      const { EmployeeID, SupervisorID } = item;
+      const node = hierarchyMap[EmployeeID];
+      const supervisorNode = hierarchyMap[SupervisorID];
+  
+      if (SupervisorID === EmployeeID) {
+        root = node;
+      } else if (supervisorNode) {
+        supervisorNode.children.push(node);
+      }
+    });
+  
+    return root;
+  };
+  
   return (
-    <>
-      <div>
-        Hello Employees
-      </div>
-      <div>
-        {data && data.data.map((item, index) => {
-          return (
-            <div key={index}>
-              {item.Ename}
-            </div>
-          )
-        })}
-      </div>
-    </>
+    <div id="treeWrapper" style={{ width: '100vw', height: '100vh'}}>
+      {data && <Tree data={data} orientation='vertical' rootNodeClassName="node__root"
+        branchNodeClassName="node__branch"
+        leafNodeClassName="node__leaf"
+        svgClassName='custom'
+         />}
+    </div>
   )
 }
 
